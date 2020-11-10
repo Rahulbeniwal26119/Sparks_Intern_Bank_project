9ebe287dd8eb6b4cf52134ada91787c06902456a
= <=>
2020-11-10 05:33:04 +0530

    adding buttons view and transfer , view profile task complete

--- php/all_customer.php
+++ php/all_customer.php
@@ -10,10 +10,13 @@ echo "<hr>";
 echo <<<_END
 <div id="container-fluid">
 <div class="row">
-<div class='col-sm-3 grid-item  py-3 title' id="title-top">Customer Name </div>
-<div class='col-sm-3 grid-item  py-3 title' id="title-email">Email </div>
-<div class='col-sm-3 grid-item  py-3 title' id="title-acc"> Acc. Number </div>
-  <p class='col-sm-3 grid-item  py-3 title' id="title-balance">Balance </p>
+<div class='col-sm-2 grid-item  py-3 title' id="title-top">Customer Name </div>
+<div class='col-sm-2 grid-item  py-3 title' id="title-email">Email </div>
+<div class='col-sm-2 grid-item  py-3 title' id="title-acc"> Acc. Number </div>
+  <p class='col-sm-2 grid-item  py-3 title' id="title-balance">Balance </p>
+  <div class='col-sm-2 grid-item  py-3 title' id="title-Transfer"> Transfer </div>
+  <div class='col-sm-2 grid-item  py-3 title' id="title-view" data-toggle="modal" data-target="#myModal"> ViewUser </div>
+
 </div>
 </div>
 _END;
@@ -22,16 +25,26 @@ echo "<hr>";
 for($i=0; $i<sizeof($local_store["name"]);$i++)
 {
   echo <<<_END
-  <div class="container-fluid detail">
+  <div class="container-fluid detail detail">
   <div class="row">
-  <div class='col-sm-3 grid-item py-3' id="name">$name[$i] </div>
-  <div class='col-sm-3 grid-item  py-3' id="email"> $email[$i] </div>
-  <div class='col-sm-3 grid-item  py-3' id="acc">$acc[$i] </div>
-    <p class='col-sm-3 grid-item  py-3' id="balance"><span>&#8360</span>. $balance[$i] </p>
+  <div class='col-sm-2 grid-item py-3 ' id="name">$name[$i] </div>
+  <div class='col-sm-2 grid-item  py-3' id="email"> $email[$i] </div>
+  <div class='col-sm-2 grid-item  py-3' id="acc">$acc[$i] </div>
+    <p class='col-sm-2 grid-item  py-3 customer${i}' id="balance"><span>&#8360</span>. $balance[$i] </p>
+    <div class='col-sm-2 grid-item  py-3' id='div-tsn${i}' > <button class="btn-primary" id="btn-transfer${i}">Transfer</div>
+    <div class='col-sm-2 grid-item  py-3' id='div-view${i}'><button class="btn-primary" id="btn-view${i}" onclick="showModal('$name[$i]' ,'$email[$i]', '$acc[$i]' , '$balance[$i]' , '$i');">ViewDetail</div>
+
   </div>
   </div>
+  <div id="modal-space"> </div>
     <hr>
   _END;
 }
+echo <<<_END
+
+<script defer type="text/javascript" src="../js/modal.js">
+
+</script>
+_END;
 // echo "</div>";
  ?>
